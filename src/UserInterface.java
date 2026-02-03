cec4c63e358acbd03eb25f2d7330c8669530e2e8
Meykaye <Meykaye.socials@gmail.com>
2026-02-03 17:13:12 +0530

    [Rishabh] UC7: retrieve vessel by ID and display formatted vessel details

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,8 +1,8 @@
 /*
- * UC5: User Interaction
- * ---------------------
- * Accepts vessel data from console
- * and stores it using VesselUtil.
+ * UC7: Display Vessel Details by ID
+ * --------------------------------
+ * Retrieves vessel details using vesselId
+ * and displays formatted output.
  */
 
 import java.util.Scanner;
@@ -14,24 +14,42 @@ public class UserInterface {
         Scanner sc = new Scanner(System.in);
         VesselUtil util = new VesselUtil();
 
+        // Step 1: Accept number of vessels
         System.out.println("Enter the number of vessels to be added");
         int n = sc.nextInt();
         sc.nextLine();
 
+        // Step 2: Accept vessel details
         System.out.println("Enter vessel details");
-
         for (int i = 0; i < n; i++) {
-            String input = sc.nextLine();
-            String[] data = input.split(":");
+            String[] data = sc.nextLine().split(":");
 
-            Vessel vessel = new Vessel(
-                    data[0],
-                    data[1],
-                    Double.parseDouble(data[2]),
-                    data[3]
+            util.addVesselPerformance(
+                    new Vessel(
+                            data[0],
+                            data[1],
+                            Double.parseDouble(data[2]),
+                            data[3]
+                    )
             );
+        }
 
-            util.addVesselPerformance(vessel);
+        // Step 3: Retrieve vessel by ID
+        System.out.println("Enter the Vessel Id to check speed");
+        String searchId = sc.nextLine();
+
+        Vessel vessel = util.getVesselById(searchId);
+
+        // Step 4: Display result
+        if (vessel != null) {
+            System.out.println(
+                    vessel.getVesselId() + " | " +
+                            vessel.getVesselName() + " | " +
+                            vessel.getVesselType() + " | " +
+                            vessel.getAverageSpeed() + " knots"
+            );
+        } else {
+            System.out.println("Vessel Id " + searchId + " not found");
         }
 
         sc.close();
